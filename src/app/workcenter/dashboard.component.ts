49e66fa850d4dad3691ed0d4764dec9b28913687
yangwu <yang.wu@igenetech.com>
2017-04-19 10:03:51 +0800

    Add workcenter dashboard component

---
+++ src/app/workcenter/dashboard.component.ts
@@ -0,0 +1,37 @@
+import {Component} from '@angular/core'
+import {ActivatedRoute, Router} from '@angular/router'
+import {EntityService} from '../entity/service'
+
+@Component({
+  selector: 'workcenter-dashboard',
+  templateUrl: './dashboard.component.html',
+})
+export class WorkcenterDashboardComponent{
+  sub: any = {}
+  objectId: string = ''
+  workcenter: any = {}
+  workcenterId: string = ''
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private entityService: EntityService,
+  ){
+    this.objectId = this.route.snapshot.params['id']
+  }
+
+  ngOnInit(){
+    this.sub = this.route.params.subscribe(params => {
+      this.workcenterId = params['id']
+      this.getWorkcenter()
+    })
+  }
+
+  getWorkcenter(){
+    this.entityService.retrieveById(this.workcenterId)
+    .subscribe(data => {
+      this.workcenter = data
+    })
+  }
+
+}
