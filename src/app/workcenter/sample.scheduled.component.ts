14fcb6a37c2fd228d4226400edbd87ce279ca363
yangwu <yang.wu@igenetech.com>
2017-04-19 11:54:32 +0800

    Add sample scheduled component

---
+++ src/app/workcenter/sample.scheduled.component.ts
@@ -0,0 +1,38 @@
+import {Component, Input} from '@angular/core'
+import {EntityService} from '../entity/service'
+
+@Component({
+  selector: 'workcenter-sample-scheduled',
+  templateUrl: './sample.scheduled.component.html',
+})
+export class WorkcenterSampleScheduledComponent{
+  @Input() workcenter
+  @Input() callback
+  sampleList: any[] = []
+
+  constructor(
+    private entityService: EntityService,
+  ){}
+
+  ngAfterViewInit(){
+    this.getSampleList()
+  }
+
+  getSampleList(){
+    this.entityService.retrieveEntity(this.workcenter.id, 'collection')
+    .subscribe(data => {
+      this.sampleList = data
+      .filter(d => {
+        return true
+      })
+      .filter(d => {
+        if (this.callback) {
+          return this.callback(d)
+        } else {
+          return true
+        }
+      })
+    })
+  }
+
+}
