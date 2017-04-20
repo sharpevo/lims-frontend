7449d6136c187cdc95b9db950ae86c78d47de2be
yangwu <yang.wu@igenetech.com>
2017-04-20 10:34:47 +0800

    Add WorkcenterSampleTerminatedComponent

---
+++ src/app/workcenter/sample.terminated.component.ts
@@ -0,0 +1,38 @@
+import {Component, Input} from '@angular/core'
+import {EntityService} from '../entity/service'
+
+@Component({
+  selector: 'workcenter-sample-terminated',
+  templateUrl: './sample.terminated.component.html',
+})
+export class WorkcenterSampleTerminatedComponent{
+  @Input() workcenter
+  @Input() callback
+  sampleList: any[] = []
+
+  constructor(
+    private entityService: EntityService,
+  ){}
+
+  ngAfterViewInit(){
+    this.getSampleList()
+  }
+
+  getSampleList(){
+    this.entityService.retrieveEntity(this.workcenter.id, 'collection')
+    .subscribe(data => {
+      this.sampleList = data
+      .filter(d => {
+        return (!d['SYS_DATE_TERMINATED'])
+      })
+      .filter(d => {
+        if (this.callback) {
+          return this.callback(d)
+        } else {
+          return true
+        }
+      })
+    })
+  }
+
+}
