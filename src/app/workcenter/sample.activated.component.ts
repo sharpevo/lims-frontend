0401c2351013c9e0491a0ce19f9f66cf3e8829b0
yangwu <yang.wu@igenetech.com>
2017-04-24 16:50:22 +0800

    Change activated sample algorithm
    Old condition focus on the current workcenter, the new one will check
    the status of previous sample.

--- src/app/workcenter/sample.activated.component.ts
+++ src/app/workcenter/sample.activated.component.ts
@@ -21,24 +21,56 @@ export class WorkcenterSampleActivatedComponent{
   }
 
   getSampleList(){
-    let operatorCode = this.workcenter['SYS_CODE'] + "_ATTR_OPERATOR"
+    let operatorCode = 'SYS_WORKCENTER_OPERATOR'
     this.entityService.retrieveEntity(this.workcenter.id, 'collection')
     .subscribe(data => {
-      this.sampleList = data
-      .filter(d => {
-        return (d['SYS_DATE_SCHEDULED']) &&
-          (d['SYS_DATE_ARRIVED']) &&
-          (!d[operatorCode]) &&
-          (!d['SYS_DATE_COMPLETED']) &&
-          (!d['SYS_DATE_TERMINATED'])
+
+      let activatedSampleList = []
+      data.forEach(d => {
+        // retrieve chained samples by the same SYS_TARGET
+        this.entityService.retrieveChainedSamples(d['SYS_TARGET'])
+        .subscribe(samples => {
+
+          // get previous sample
+          let index = -1
+          let previousSample = {}
+
+          for (let i=0; i < samples.length; i ++){
+            if (samples[i].id == d.id){
+              index = i
+              break
+            }
+          }
+
+          if (index > -1){
+            if (index > 0){
+              previousSample = samples[index-1]
+            } else {
+              // d is the first sample in the chain and should be removed out
+              // of the scheduled list and moved into the activated list
+              previousSample = {}
+              if (!d[operatorCode]){
+                activatedSampleList.push(d)
+              }
+            }
+
+          } else {
+            console.log("samples no in the chain.")
+          }
+
+          // clear samples without operator in current workcenter
+          if (!d[operatorCode]) {
+
+            // previous sample should have been completed in some form
+            if (previousSample['SYS_DATE_COMPLETED'] ||
+                previousSample['SYS_DATE_TERMINATED']){
+              activatedSampleList.push(d)
+            }
+          }
+
+        })
       })
-      .filter(d => {
-        if (this.callback) {
-          return this.callback(d)
-        } else {
-          return true
-        }
-      })
+      this.sampleList = activatedSampleList
     })
   }
 
