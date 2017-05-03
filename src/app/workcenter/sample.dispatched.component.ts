e9a71b010ff75f2359b5c0ac242b7913c2179ff8
yangwu <yang.wu@igenetech.com>
2017-05-03 13:41:58 +0800

    Add getPreviousChainedSample in dispatched component

--- src/app/workcenter/sample.dispatched.component.ts
+++ src/app/workcenter/sample.dispatched.component.ts
@@ -1,5 +1,6 @@
 import {Component, Input} from '@angular/core'
 import {EntityService} from '../entity/service'
+import {SampleService} from '../models/sample'
 
 @Component({
   selector: 'workcenter-sample-dispatched',
@@ -14,6 +15,7 @@ export class WorkcenterSampleDispatchedComponent{
 
   constructor(
     private entityService: EntityService,
+    private sampleService: SampleService,
   ){}
 
   ngOnInit(){
@@ -46,52 +48,25 @@ export class WorkcenterSampleDispatchedComponent{
     this.entityService.retrieveEntity(this.workcenter.id, 'collection')
     .subscribe(data => {
 
-      //let dispatchedSampleList = []
       data.forEach(d => {
-        // retrieve chained samples by the same SYS_TARGET
-        this.entityService.retrieveChainedSamples(d['SYS_TARGET'])
-        .subscribe(samples => {
+        this.sampleService.getPreviousChainedSample(d,previousSample => {
 
-          // get previous sample
-          let index = -1
-          let previousSample = {}
-
-          for (let i=0; i < samples.length; i ++){
-            if (samples[i].id == d.id){
-              index = i
-              break
+          if (previousSample.id == d.id){
+            previousSample = {}
+            if (d[operatorCode] &&
+                !d['SYS_DATE_COMPLETED']) {
+              d['TMP_NEXT_SAMPLE_ID'] = d.id
+            this.sampleList.push(d)
             }
-          }
-
-          if (index > -1){
-            if (index > 0){
-              previousSample = samples[index-1]
-              // clear samples without operator in current workcenter
-              if (d[operatorCode] &&
-                  !d['SYS_DATE_COMPLETED']) {
-                previousSample['TMP_NEXT_SAMPLE_ID'] = d.id
-              this.sampleList.push(previousSample)
-              }
-
-            } else {
-              // d is the first sample in the chain and should be removed out
-              // of the scheduled list and moved into the activated list
-              previousSample = {}
-              if (d[operatorCode] &&
-                  !d['SYS_DATE_COMPLETED']) {
-                d['TMP_NEXT_SAMPLE_ID'] = d.id
-              this.sampleList.push(d)
-              }
-            }
-
           } else {
-            console.log("samples no in the chain.")
+            if (d[operatorCode] &&
+                !d['SYS_DATE_COMPLETED']) {
+              previousSample['TMP_NEXT_SAMPLE_ID'] = d.id
+            this.sampleList.push(previousSample)
+            }
           }
-
         })
       })
-      //this.sampleList = dispatchedSampleList
     })
   }
-
 }
