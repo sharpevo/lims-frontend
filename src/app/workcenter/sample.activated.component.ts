8032065d4c52a0e5875e679df22ab54f2c7e36e4
yangwu <yang.wu@igenetech.com>
2017-05-04 10:55:21 +0800

    Add observable in sample activated component

--- src/app/workcenter/sample.activated.component.ts
+++ src/app/workcenter/sample.activated.component.ts
@@ -1,6 +1,7 @@
 import {Component, Input} from '@angular/core'
 import {EntityService} from '../entity/service'
 import {SampleService} from '../models/sample'
+import {Observable} from 'rxjs/Observable'
 
 @Component({
   selector: 'workcenter-sample-activated',
@@ -12,6 +13,7 @@ export class WorkcenterSampleActivatedComponent{
   @Input() checkedEntityList
 
   sampleList: any[] = []
+  loaded: boolean = false
 
   constructor(
     private entityService: EntityService,
@@ -25,29 +27,42 @@ export class WorkcenterSampleActivatedComponent{
   getSampleList(){
     this.sampleList = []
     let operatorCode = 'SYS_WORKCENTER_OPERATOR'
+    let chainedSampleObs = []
 
     this.entityService.retrieveEntity(this.workcenter.id, 'collection')
-    .subscribe(data => {
+    .subscribe(samples => {
 
       let activatedSampleList = []
-      data.forEach(d => {
+      samples.forEach(d => {
+        chainedSampleObs.push(
+          this.entityService.retrieveBy({
+            'SYS_TARGET': d['SYS_TARGET'],
+            'sort': 'SYS_ORDER'})
+        )
+      })
 
-        this.sampleService.getPreviousChainedSample(d, previousSample => {
 
-          // clear samples without operator in current workcenter
-          if (!d[operatorCode]) {
+      Observable
+      .forkJoin(chainedSampleObs)
+      .subscribe((data: any[][]) => {
+
+        for (let i=0; i<data.length; i++){
+          let previousSample = this.sampleService.parsePreviousSample(samples[i], data[i])
+          if (!samples[i][operatorCode]) {
 
             // previous sample should have been completed in some form
             if (previousSample['SYS_DATE_COMPLETED'] ||
                 previousSample['SYS_DATE_TERMINATED']){
               // push previous sample in the avalable list to get attributes
-              previousSample['TMP_NEXT_SAMPLE_ID'] = d.id
+              previousSample['TMP_NEXT_SAMPLE_ID'] = samples[i].id
               this.sampleList.push(previousSample)
             }
           }
-        })
+        }
+
 
       })
+
     })
   }
 }
