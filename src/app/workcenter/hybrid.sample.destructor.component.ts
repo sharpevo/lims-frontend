63b9cc4d0c6fc6ce992ab1bec93206cd3200d8b0
yangwu <yang.wu@igenetech.com>
2017-05-11 15:12:43 +0800

    Add checkSample func in HybridSampleDestructorComponent

--- src/app/workcenter/hybrid.sample.destructor.component.ts
+++ src/app/workcenter/hybrid.sample.destructor.component.ts
@@ -8,11 +8,38 @@ import {Component, Input} from '@angular/core'
 export class HybridSampleDestructorComponent {
   @Input() hybridSample
   showElement: boolean = false
+  hybridSampleChecked: boolean = false
+  maxLoop: number = 5
+  sampleList: any[]
   constructor(){
   }
 
   ngOnInit(){
-    console.log(this.hybridSample)
   }
 
+  getSampleList(hybridSample: any){
+    if (this.maxLoop < 0) {
+      console.log("not valid samples")
+      return
+    }
+    let keys = Object.keys(hybridSample)
+    console.log(this.maxLoop)
+    if (keys[0] != 'SAMPLES'){
+      this.maxLoop -= 1
+      keys.forEach(key => {
+        this.getSampleList(hybridSample[key])
+      })
+    } else {
+      this.maxLoop = 5
+      this.sampleList = hybridSample['SAMPLES']
+    }
+  }
+
+  checkSample(){
+    this.getSampleList(this.hybridSample)
+    this.sampleList.forEach(sample => {
+      sample['TMP_CHECKED'] = !this.hybridSampleChecked
+    })
+
+  }
 }
