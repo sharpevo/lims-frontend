f0e2abcd6f4109c86dc254a850a751fa324fe084
yangwu <yang.wu@igenetech.com>
2017-05-26 15:15:38 +0800

    Add index validator plugin

---
+++ src/app/plugins/index.validator.ts
@@ -0,0 +1,86 @@
+import {Component, Input} from '@angular/core'
+import {EntityService} from '../entity/service'
+import {SampleService} from '../models/sample'
+
+@Component({
+  selector: 'plugin-index-validator',
+  templateUrl: './index.validator.html',
+})
+export class PluginIndexValidatorComponent {
+  @Input() sampleList
+  selectedSampleList: any[] = []
+  result: boolean = true
+  previousCheckedList: boolean[] = []
+  resultList: boolean[] = []
+
+  constructor(
+    private entityService: EntityService,
+    private sampleService: SampleService,
+  ) {}
+
+  ngOnInit(){
+    this.updatePreviousCheckedList()
+    console.log(this.previousCheckedList)
+  }
+
+  ngDoCheck(){
+    if (this.isSampleListChanged()){
+      this.validateIndices()
+    }
+  }
+
+  updatePreviousCheckedList(){
+    this.previousCheckedList = []
+    for (let i=0; i<this.sampleList.length; i++){
+      let sample = this.sampleList[i]
+      if (sample['TMP_CHECKED']){
+        this.previousCheckedList.push(true) 
+      } else {
+        this.previousCheckedList.push(false) 
+      }
+    }
+  }
+
+  isSampleListChanged(): boolean{
+    for (let i=0; i<this.sampleList.length; i++){
+      let sample = this.sampleList[i]
+      if (sample['TMP_CHECKED'] != null &&
+          sample['TMP_CHECKED'] != this.previousCheckedList[i]){
+        console.log("==", sample['TMP_CHECKED'], this.previousCheckedList[i])
+      this.updatePreviousCheckedList()
+      return true
+      }
+    }
+    return false
+  }
+
+  validateIndices(){
+    this.selectedSampleList = this.sampleList.filter(sample => sample.TMP_CHECKED)
+    this.resultList = []
+    if (this.selectedSampleList.length == 0){
+      return
+    }
+    let seqMap = {}
+    //for (let sample of this.selectedSampleList; let i = index){
+    for (let i=0; i<this.selectedSampleList.length; i++){
+      let key = this.selectedSampleList[i]['SYS_INDEX_SEQUENCE']
+      //console.log(key)
+      //console.log(seqMap[key])
+      //if (seqMap[sample['SYS_INDEX_SEQUENCE']] === '') {
+      if (seqMap[key] == null) {
+        // id is more stable than SYS_SAMLPE_CODE
+        // or index note that 0=='' returns true
+        seqMap[key] = ''+i
+        this.resultList[i] = true
+      } else {
+        this.result = false
+        this.resultList[i] = false
+        this.resultList[Number(seqMap[key])] = false
+        return
+      }
+    }
+    this.result = true
+
+  }
+
+}
