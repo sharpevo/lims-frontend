b4edc1eced3587824f7c9f59b1cea51e671b185e
yangwu <yang.wu@igenetech.com>
2017-09-14 11:55:13 +0800

    Move parentMap generation logics to ngOnInit

--- src/app/plugins/excel.processor.ts
+++ src/app/plugins/excel.processor.ts
@@ -14,12 +14,57 @@ export class PluginExcelProcessorComponent {
   @ViewChild('excelUploader') excelUploader
   selectedSampleList: any[] = []
   excelResult: any[] = []
+  parentMap: any = {} // parent entity like BoM or Routing
+  entityMap: any = {} // entity type like operator
+  workcenterAttributeList: any[] = []
   constructor(
     private utilService: UtilService,
     private entityService: EntityService,
     private genreService: GenreService,
     private sampleService: SampleService,
   ){}
+
+  ngOnInit(){
+    this.generateParentMap()
+  }
+
+  generateParentMap(){
+    // Get the genre of given workcenter
+    this.entityService.retrieveGenre(this.workcenter.id)
+    .subscribe(data => {
+
+      // Get the attributes from the first genre
+      this.genreService.retrieveAttribute(data[0].id)
+      .subscribe(attributeList => {
+        this.workcenterAttributeList = attributeList
+        this.workcenterAttributeList.forEach(attr => {
+
+          console.log("..")
+
+          // Process BoM or Routing
+          if (attr.SYS_TYPE == 'entity' && !attr.SYS_TYPE_ENTITY_REF) {
+            this.parentMap[attr.SYS_CODE] = {}
+
+            // Get the entities under the BoM, note that the empty string indicates
+            // the "object" entity type which is implemented in the entityService.
+            this.entityService.retrieveEntity(attr.SYS_TYPE_ENTITY.id, "")
+            .subscribe(data => {
+              data.forEach(material => {
+                this.parentMap[attr.SYS_CODE][material.id] = {}
+                material['SYS_SCHEMA'].forEach(materialAttr => {
+                  this.parentMap[attr.SYS_CODE][material.id][materialAttr.SYS_CODE] =
+                    material[materialAttr.SYS_CODE]
+                })
+                // Manually append the SYS_FLOOR_ENTITY_TYPE in BoM/Routing entry
+                this.parentMap[attr.SYS_CODE][material.id]['SYS_FLOOR_ENTITY_TYPE'] =
+                  attr.SYS_FLOOR_ENTITY_TYPE
+              })
+            })
+          }
+        })
+      })
+    })
+  }
 
   submitExcel(event){
     let file = event.srcElement.files;
@@ -43,68 +88,56 @@ export class PluginExcelProcessorComponent {
 
   updateExcel(){
 
-    // Get the genre of given workcenter
-    this.entityService.retrieveGenre(this.workcenter.id)
-    .subscribe(data => {
+    this.excelResult.forEach(sample =>{
 
-      // Get the attributes from the first genre
-      this.genreService.retrieveAttribute(data[0].id)
-      .subscribe(workcenterAttributeList => {
-        workcenterAttributeList.forEach(attr => {
+      // Convert excel-style object to database-style object.
+      // The excel-style object is formed as:
+      // 'LABEL': 'Value'
+      this.entityService.retrieveBy({
+        "_id": sample.IDENTIFIER
+      })
+      .subscribe(data => {
+        let mergedSample = data[0]
 
-          console.log("..")
-          let parentMap = {}
-          if (attr.SYS_TYPE == 'entity' && !attr.SYS_TYPE_ENTITY_REF) {
-            parentMap[attr.SYS_CODE] = {}
+        //
+        // For the reason of SYS_GENRE is defined from General project instead
+        // of the current workcenter in the legacy database:
+        // 1. schema: from workcenter, like sample sn
+        // 2. sample: from excel, like operator, id, exactly the workcenter
+        //
+        mergedSample.SYS_SCHEMA.forEach(schema => {
+          console.log("!!!", sample, schema.SYS_LABEL)
+          if (sample[schema.SYS_LABEL]){
+            if (schema.SYS_TYPE != 'entity'){
+              mergedSample[schema.SYS_CODE] = sample[schema.SYS_LABEL]
+            } else {
 
-            // Get the entities under the BoM, note that the empty string indicates
-            // the "object" entity type which is implemented in the entityService.
-            this.entityService.retrieveEntity(attr.SYS_TYPE_ENTITY.id, "")
-            .subscribe(data => {
-              data.forEach(material => {
-                parentMap[attr.SYS_CODE][material.id] = {}
-                material['SYS_SCHEMA'].forEach(materialAttr => {
-                  parentMap[attr.SYS_CODE][material.id][materialAttr.SYS_CODE] =
-                    material[materialAttr.SYS_CODE]
-                })
-                // Manually append the SYS_FLOOR_ENTITY_TYPE in BoM/Routing entry
-                parentMap[attr.SYS_CODE][material.id]['SYS_FLOOR_ENTITY_TYPE'] =
-                  attr.SYS_FLOOR_ENTITY_TYPE
+              // Convert SYS_LABEL to id before commit to the database
+              let queryObject = {}
+              queryObject[schema.SYS_LABEL] = sample[schema.SYS_LABEL]
+              this.entityService.retrieveBy(queryObject)
+              .subscribe(data => {
+                console.log("&&&", data)
+                if (data[0]){
+                  console.log("Entity:", data[0])
+                  mergedSample[schema.SYS_CODE] = data[0].id
+                } else {
+                  console.warn("Invalid " + schema.SYS_LABEL + sample[schema.SYS_LABEL])
+                }
               })
-              console.log("xx", workcenterAttributeList)
-              this.excelResult.forEach(sample =>{
-
-                // Convert excel-style object to database-style object.
-                // The excel-style object is formed as:
-                // 'LABEL': 'Value'
-                this.entityService.retrieveBy({
-                  "_id": sample.IDENTIFIER
-                })
-                .subscribe(data => {
-                  let mergedSample = data[0]
-
-                  mergedSample.SYS_SCHEMA.forEach(schema => {
-                    if (sample[schema.SYS_LABEL]){
-                      mergedSample[schema.SYS_CODE] = sample[schema.SYS_LABEL]
-                    }
-                  })
-                  this.sampleService.submitSample(
-                    this.workcenter,
-                    mergedSample,
-                    data[0],
-                    {
-                      "attributeList": workcenterAttributeList,
-                      "parentMap": parentMap
-                    })
-                })
-              })
-            })
+            }
           }
         })
+        this.sampleService.submitSample(
+          this.workcenter,
+          mergedSample,
+          data[0],
+          {
+            "attributeList": this.workcenterAttributeList,
+            "parentMap": this.parentMap
+          })
       })
     })
-
-
   }
 
   updateExcel2(){
