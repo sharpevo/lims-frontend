7949c9d1691e24d3b56349794571473be6fe3f19
yangwu <yang.wu@igenetech.com>
2017-10-31 09:55:14 +0800

    Add setCookie for bootstrap component
    Issue: #50

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import {EntityService} from './entity/service'
 import {environment} from '../environments/environment'
+import {URLSearchParams} from "@angular/http"
 
 @Component({
   selector: 'app-root',
@@ -17,7 +18,19 @@ export class AppComponent {
 
   ngOnInit(){
     this.getServiceList()
+    this.getParams()
   }
+
+  getParams() {
+    let params = new URLSearchParams(window.location.search)
+    let remember = params.get('?remember')
+    let token = params.get('token')
+
+    if (token) {
+      this.setCookie("token", token, remember)
+    }
+  }
+
   getServiceList(){
     this.entityService.retrieveByType("domain")
     .subscribe(
@@ -30,5 +43,19 @@ export class AppComponent {
   setLanguage(language: string) {
     localStorage.setItem('locale', language)
     location.reload(true)
+  }
+
+  setCookie(name: string, value: string, remember: string, path: string = ""){
+    let date = new Date()
+    let minutes: number
+    if (remember == "true"){
+      minutes = 7200 // 5 * 24 * 60
+    } else {
+      minutes = 30
+    }
+    date.setTime(date.getTime() + minutes * 60 * 1000)
+    let expires = "expires=" + date.toUTCString()
+    document.cookie = name + "=" + value + "; " +  expires + (path.length > 0 ? "; path=" + path : "")
+    console.log("cookie", document.cookie)
   }
 }
