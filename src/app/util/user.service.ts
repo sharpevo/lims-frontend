39014c8ba89bca9a4cb5e515c78ed926623fb90c
yangwu <yang.wu@igenetech.com>
2017-11-16 13:08:04 +0800

    Return cached user info or make requests to get it
    Return an observable that emits the correct data before/during/after the first
    request to the server
    
    Issue:#50

--- src/app/util/user.service.ts
+++ src/app/util/user.service.ts
@@ -1,6 +1,9 @@
 import {Injectable} from '@angular/core'
-import {Subject} from 'rxjs/Subject'
+//import {Subject} from 'rxjs/Subject'
+import {Observable} from 'rxjs/Observable'
+import 'rxjs/add/observable/of'
 import 'rxjs/add/operator/share'
+import 'rxjs/add/operator/map'
 import {UtilService} from './service'
 import {EntityService} from '../entity/service'
 import {SpinnerService} from './spinner.service'
@@ -9,7 +12,8 @@ import {environment} from '../../environments/environment'
 
 @Injectable()
 export class UserService {
-  private userInfo = new Subject()
+  userInfo: any
+  observable: Observable<any>
   environment = environment
   constructor(
     private spinnerService: SpinnerService,
@@ -19,9 +23,54 @@ export class UserService {
   ){}
 
 
+
+  // never process auth fail in it
+  getUserInfo(){
+    console.log("get user info")
+    if (this.userInfo) {
+      return Observable.of(this.userInfo)
+    } else if (this.observable) {
+      console.log("this observable")
+      return this.observable
+    } else {
+      console.log("retrieve")
+      return this.utilService.getUserInfo()
+      .catch(error => {
+        console.log("invalid user login")
+        return Observable.of(false) // avid the empty error for `take(1)`
       })
+      .mergeMap(userInfo => {
+        console.log("user info raw", userInfo)
+        this.userInfo = userInfo
+        this.userInfo['role'] = JSON.parse(userInfo['role'])
+        return this.entityService.retrieveBy({
+          "SYS_USER_EMAIL": userInfo.email
+        })
+        .map(data => {
+          this.observable = null
+          if (data.length > 0){
+            this.userInfo['limsid'] = data[0]['id']
+          }
+          console.log("user info updated", this.userInfo)
+          return this.userInfo
+        })
       })
+      .share()
+      .catch(error => {
+        console.log("illegal user")
+        return Observable.of(false)
+      })
+    }
   }
 
+  authFail(){
+    this.spinnerService.start()
+    this.snackBar.open("Redirect to UIC in 3 seconds...", "OK", {duration: 3000})
+    .afterDismissed().subscribe(() => {
+      this.spinnerService.stop()
+      window.location.href = environment.uicUrl +
+        "/login?return_to=" +
+        environment.limsUrl.replace(/^https?:\/\//,'')
+    })
   }
 }
