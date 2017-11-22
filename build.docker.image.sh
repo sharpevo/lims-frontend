01d4eb316e8a452d06c8f2eb164c51d6163c1510
yangwu <yang.wu@igenetech.com>
2017-11-22 09:52:00 +0800

    Add git commit in docker images
    Issue: #59

--- build.docker.image.sh
+++ build.docker.image.sh
@@ -13,6 +13,9 @@ image_file=$image_folder/$app.${version}_${timestamp}
 echo ">>> building app: $version"
 ng build --environment $version --aot --locale zh-CN --i18n-file src/locale/messages.zh-CN.xlf --i18n-format xlf
 echo ">>> building images: $image_tag"
-docker build -t $image_tag .
+docker build \
+    -t $image_tag \
+    --build-arg GIT_COMMIT=$(git log -1 --format=%h) \
+    .
 echo ">>> saving images: $image_file"
 docker save -o $image_file $image_tag
