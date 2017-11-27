9f9b7470f92c8bd4ce95ca0538d0facae0a2409c
yangwu <yang.wu@igenetech.com>
2017-11-27 13:58:41 +0800

    Add build number to the toolbar
    Issue: #59

--- build.docker.image.sh
+++ build.docker.image.sh
@@ -2,6 +2,7 @@
 image_folder="docker"
 app="lims-frontend"
 timestamp=`date +%Y%m%d_%H%M`
+git_commit=`git log -1 --format=%h`
 if [ -z "$1" ]
 then
     version="test"
@@ -10,12 +11,14 @@ else
 fi
 image_tag=$app:${version}_${timestamp}
 image_file=$image_folder/$app.${version}_${timestamp}
-echo ">>> building app: $version"
+build_number="version: \"Build ${git_commit} | ${version} ${timestamp}\","
+echo ">>> building app: $build_number"
+sed -i -e "s/version.*,/$build_number/" ./src/environments/environment.$version.ts
 ng build --environment $version --aot --locale zh-CN --i18n-file src/locale/messages.zh-CN.xlf --i18n-format xlf
 echo ">>> building images: $image_tag"
 docker build \
     -t $image_tag \
-    --build-arg GIT_COMMIT=$(git log -1 --format=%h) \
+    --build-arg GIT_COMMIT=${git_commit}\
     .
 echo ">>> saving images: $image_file"
 docker save -o $image_file $image_tag
