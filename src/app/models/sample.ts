3c1b13ac63c08f8d0f8a237f0efe9ed5755a1bf4
yangwu <yang.wu@igenetech.com>
2018-01-12 14:28:24 +0800

    Refactor getAuxiliaryAttributes
    Issue: #181

--- src/app/models/sample.ts
+++ src/app/models/sample.ts
@@ -154,6 +154,60 @@ export class SampleService{
       callback(attributeObjectList)
     })
   }
+  getAuxiliaryAttributes(sample: any, sampleSet: any[], attributeCode: string, attributeGenre: string){
+
+    let sampleList = sampleSet
+    .sort((a,b) => {
+      //if (a['updatedAt'] < b['updatedAt']){
+      if (a['SYS_DATE_COMPLETED'] < b['SYS_DATE_COMPLETED']){
+        return 1
+      } else {
+        return -1
+      }
+    })
+    let attributeObjectList = []
+
+    let activatedSampleList = sampleList
+    .filter(sample => sample['SYS_DATE_COMPLETED'])// &&
+    //!sample['SYS_DATE_TERMINATED'])
+    if (activatedSampleList.length > 0){
+      let uniqueSampleList = []
+      let seen = {}
+      activatedSampleList.forEach(sample => {
+        let key = attributeCode == 'SYS_SAMPLE_CODE'?attributeCode:attributeCode + "|" + sample['SYS_GENRE'] + sample[attributeCode]
+        if (!seen[key]) {
+          if (sample[attributeCode]){
+            seen[key] = true
+          }
+
+          if (attributeGenre == sample['SYS_GENRE'] || attributeCode == "SYS_SAMPLE_CODE") {
+            uniqueSampleList.push(sample)
+          }
+        }
+      })
+
+      uniqueSampleList
+      .forEach(sample => {
+        attributeObjectList.push({
+          "id": sample.id,
+          "dateCompleted": sample['SYS_DATE_COMPLETED'],
+          "dateUpdated": sample['updatedAt'],
+          "value": sample[attributeCode]?sample[attributeCode]:"---"
+        })
+      })
+    } else {
+      // For samples that are just submitted, none of which satisfied the
+      // date condition, so push the attributes of the first sample.
+      let firstSample = sampleList[0]
+      attributeObjectList.push({
+        "id": firstSample.id,
+        "dateCompleted": firstSample['SYS_DATE_COMPLETED'],
+        "dateUpdated": firstSample['updatedAt'],
+        "value": firstSample[attributeCode]?firstSample[attributeCode]:"---"
+      })
+    }
+    return attributeObjectList
+  }
 
   retrieveAuxiliaryAttributeList(sample: any, attributeCode: string, attributeGenre: string){
 
