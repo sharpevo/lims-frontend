2e09cbf28a61d3f1720fe1115ebf9dec4910ebdb
yangwu <yang.wu@igenetech.com>
2018-03-12 17:28:31 +0800

    Remove HttpClientModule

--- src/app/app.load.module.ts
+++ src/app/app.load.module.ts
@@ -1,6 +1,4 @@
 import { NgModule, APP_INITIALIZER } from '@angular/core';
-import { HttpClientModule } from "@angular/common/http";
-
 import { AppLoadService } from './app.load.service';
 
 export function init_app(appLoadService: AppLoadService) {
@@ -8,7 +6,6 @@ export function init_app(appLoadService: AppLoadService) {
 }
 
 @NgModule({
-  imports: [HttpClientModule],
   providers: [
     AppLoadService,
     { provide: APP_INITIALIZER, useFactory: init_app, deps: [AppLoadService], multi: true },
