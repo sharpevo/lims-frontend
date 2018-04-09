0fbf09140207bd930794404c075cde0524680df3
yangwu <yang.wu@igenetech.com>
2018-04-09 21:33:24 +0800

    Use LogService in UserInfoService
    Issue: #228

--- src/app/util/user.info.service.ts
+++ src/app/util/user.info.service.ts
@@ -1,24 +1,28 @@
 import {Injectable} from '@angular/core'
+import {LogService, LogLevel} from '../log/log.service'
 
 @Injectable()
 export class UserInfoService {
   userInfo: any
-  constructor (){}
+    constructor (
+        public logger: LogService
+    ){}
   setUserInfo(userInfo: any) {
     this.userInfo = userInfo
   }
 
   getUserInfo() {
-    console.log("get userinfo", this.userInfo)
+        this.logger.debug("UserInfo", this.userInfo)
     return this.userInfo
   }
 
   hasRole(role: string): boolean{
+        this.logger.debug("CheckPerm", role)
     if (!this.userInfo) {
       return false
     }
     if (this.userInfo.email == "quwubin@gmail.com") {
-      console.log("Check role: super admin")
+            this.logger.debug("CheckPerm", "Super admin")
       return true
     }
     return this.userInfo.role[role]
