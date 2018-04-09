dde6863c1827b714c59bee2b3e94178478be29be
yangwu <yang.wu@igenetech.com>
2018-04-09 15:21:51 +0800

    Add log services
    Issue: #228

---
+++ src/app/log/publisher.ts
@@ -0,0 +1,23 @@
+import {Observable} from 'rxjs/Observable'
+import 'rxjs/add/observable/of'
+import {LogEntry} from './log.service'
+
+export abstract class LogPublisher {
+    location: string
+    abstract log(record: LogEntry): Observable<boolean>
+    abstract clear(): Observable<boolean>
+}
+
+export class LogConsole extends LogPublisher {
+
+    log(record: LogEntry): Observable<boolean> {
+        console.log(record.buildLogString())
+        return Observable.of(true)
+    }
+
+    clear(): Observable<boolean> {
+        console.clear()
+        return Observable.of(true)
+    }
+
+}
