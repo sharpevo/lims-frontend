e79db818c9d64cbd1b467d217877f461189b0e4f
yangwu <yang.wu@igenetech.com>
2018-04-10 09:57:16 +0800

    Pretty format for logs
    Issue: #228

--- src/app/log/publisher.ts
+++ src/app/log/publisher.ts
@@ -1,6 +1,7 @@
 import {Observable} from 'rxjs/Observable'
 import 'rxjs/add/observable/of'
 import {LogEntry} from './log.service'
+import {LogLevel} from './log'
 
 export abstract class LogPublisher {
     location: string
@@ -9,10 +10,43 @@ export abstract class LogPublisher {
 }
 
 export class LogConsole extends LogPublisher {
+    debugStyle = [
+        "background-color: #d3d7cf",
+        "color: white",
+    ].join(";")
+    infoStyle = [
+        "background-color: #729fcf",
+        "color: white",
+    ].join(";")
+    warnStyle = [
+        "background-color: #fcaf3e",
+        "color: white",
+    ].join(";")
+    errorStyle = [
+        "background-color: #ef2929",
+        "color: white",
+    ].join(";")
 
     log(record: LogEntry): Observable<boolean> {
-        //console.log(record.buildLogString())
-        console.log(...record.buildLogObject())
+
+        switch (record.level) {
+            case (LogLevel.DEBUG):
+                console.log("%c %s", this.debugStyle, ...record.buildLogObject())
+            break
+            case (LogLevel.INFO):
+                console.log("%c %s", this.infoStyle, ...record.buildLogObject())
+            break
+            case (LogLevel.WARN):
+                console.log("%c %s", this.warnStyle, ...record.buildLogObject())
+            break
+            case (LogLevel.ERROR):
+                console.log("%c %s", this.errorStyle, ...record.buildLogObject())
+            break
+            default:
+                console.log(...record.buildLogObject())
+            break
+        }
+        //console.log(...record.buildLogObject())
         return Observable.of(true)
     }
 
