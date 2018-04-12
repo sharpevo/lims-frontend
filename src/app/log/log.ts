8af72348e273ae3c988cd46831c58c6c3cae8ec7
yangwu <yang.wu@igenetech.com>
2018-04-12 13:43:06 +0800

    Change indentation by tsfmt

--- src/app/log/log.ts
+++ src/app/log/log.ts
@@ -60,11 +60,11 @@ export class LogEntry {
 
 export enum LogLevel {
     ALL = 0,
-        DEBUG = 1,
-        INFO = 2,
-        WARN = 3,
-        ERROR = 4,
-        FATAL = 5,
-        OFF = 6
+    DEBUG = 1,
+    INFO = 2,
+    WARN = 3,
+    ERROR = 4,
+    FATAL = 5,
+    OFF = 6
 }
 
