e0c1c368edd18da312f00a65691be9d9e23b9f59
yangwu <yang.wu@igenetech.com>
2018-04-28 10:11:15 +0800

    Add MatGridListModule

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -22,12 +22,12 @@ import {
     MatSelectModule,
     MatChipsModule,
     MatTableModule,
-    MatGridListModule,
     MatSidenavModule,
     MatCheckboxModule
 } from '@angular/material';
 import {MatDividerModule} from '@angular/material/divider';
 import {MatTabsModule} from '@angular/material/tabs';
+import {MatGridListModule} from '@angular/material/grid-list';
 
 @NgModule({
     imports: [
