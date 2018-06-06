95bc63f9e71def2f8bc4f5fe597b92c4ad14dfc3
yangwu <yang.wu@igenetech.com>
2018-06-06 17:30:31 +0800

    Change indentation by tsfmt

--- src/app/entity/tostring.component.ts
+++ src/app/entity/tostring.component.ts
@@ -2,61 +2,61 @@ import {Component, Input} from '@angular/core'
 import {EntityService} from './service'
 
 @Component({
-  selector: 'entity-tostring',
-  templateUrl: './tostring.component.html',
+    selector: 'entity-tostring',
+    templateUrl: './tostring.component.html',
 })
 export class EntityToStringComponent {
-  @Input('entity') entity
-  @Input('entityId') entityId
-  label: string = ""
-  constructor(private entityService: EntityService) {}
+    @Input('entity') entity
+    @Input('entityId') entityId
+    label: string = ""
+    constructor(private entityService: EntityService) {}
 
-  ngOnInit(){
-    if (this.entityId){
-      this.entityService.retrieveBy({
-        "_id": this.entityId, // "id" does not exist in database
-      }).subscribe(data => {
-        this.entity = data[0]
-        this.getEntityLabel()
-      })
-    } else if (this.entity){
-      this.getEntityLabel()
-    } else {
-      this.label = "---"
+    ngOnInit() {
+        if (this.entityId) {
+            this.entityService.retrieveBy({
+                "_id": this.entityId, // "id" does not exist in database
+            }).subscribe(data => {
+                this.entity = data[0]
+                this.getEntityLabel()
+            })
+        } else if (this.entity) {
+            this.getEntityLabel()
+        } else {
+            this.label = "---"
+        }
     }
-  }
 
-  getEntityLabel(){
+    getEntityLabel() {
         if (!this.entity) {
             this.label = this.entity || this.entityId
             return
         }
-    // Get all the attributes and check whether the `SYS_IS_ENTITY_LABEL` is
-    // true, which is assigned while the creation of the attribute
-    this.entityService.retrieveAttribute(this.entity.id)
-    .subscribe(attributes => {
+        // Get all the attributes and check whether the `SYS_IS_ENTITY_LABEL` is
+        // true, which is assigned while the creation of the attribute
+        this.entityService.retrieveAttribute(this.entity.id)
+            .subscribe(attributes => {
 
-      // As default, there's some entity does not have any predefined
-      // attribute, but has an `label` attribute which will be used as label
-      // in preference to the id
-      if (this.entity['SYS_LABEL']){
-        this.label = this.entity[this.entity['SYS_LABEL']]
-      } else if (this.entity['label']){
-        this.label = this.entity.label
-      } else {
-        this.label = this.entity.id
-      }
+                // As default, there's some entity does not have any predefined
+                // attribute, but has an `label` attribute which will be used as label
+                // in preference to the id
+                if (this.entity['SYS_LABEL']) {
+                    this.label = this.entity[this.entity['SYS_LABEL']]
+                } else if (this.entity['label']) {
+                    this.label = this.entity.label
+                } else {
+                    this.label = this.entity.id
+                }
 
-      if (!this.label) {
-        // No way to break Angular forEach, so native for-loop is better
-        for (let attribute of attributes){
-          if (attribute['SYS_IS_ENTITY_LABEL']) {
-            this.label = this.entity[attribute['SYS_CODE']]
-            return
-          }
-        }
-      }
+                if (!this.label) {
+                    // No way to break Angular forEach, so native for-loop is better
+                    for (let attribute of attributes) {
+                        if (attribute['SYS_IS_ENTITY_LABEL']) {
+                            this.label = this.entity[attribute['SYS_CODE']]
+                            return
+                        }
+                    }
+                }
 
-    })
-  } 
+            })
+    }
 }
